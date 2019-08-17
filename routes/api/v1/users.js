d392be56f4349d78973f133cad5fdfe0dd82e337
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-08-16 18:22:22 -0600

    Add if logic for when passwords do not match when POSTing a new User

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -3,18 +3,23 @@ var router = express.Router();
 var User = require('../../../models').User
 
 router.post('/', function(req, res, next) {
-  User.create({
-    email: req.body.email,
-    password: req.body.password
-  })
-  .then(user => {
+  if (req.body.password == req.body.password_confirmation) {
+    User.create({
+      email: req.body.email,
+      password: req.body.password
+    })
+    .then(user => {
+      res.setHeader('Content-Type', 'application/json')
+      res.status(201).send(JSON.stringify({api_key: user.api_key}))
+    })
+    .catch(error => {
+      res.setHeader('Content-Type', 'application/json')
+      res.status(500).send({error})
+    })
+  } else {
     res.setHeader('Content-Type', 'application/json')
-    res.status(201).send(JSON.stringify({api_key: user.api_key}))
-  })
-  .catch(error => {
-    res.setHeader('Content-Type', 'application/json')
-    res.status(500).send({error})
-  })
+    res.status(409).send({message: 'Passwords do not match.'})
+  }
 });
 
 module.exports = router;
