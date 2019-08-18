95da8cedfff3e89785bd1cef59b67fc40d5f75a0
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-08-18 15:36:20 -0600

    Add #geocode and #_processed functions to GoogleMaps API object to get lat, long, formatted address for a location

--- services/google_maps_api.js
+++ services/google_maps_api.js
@@ -1,14 +1,30 @@
 require('dotenv').config()
-const googleMapsClient = require('@google/maps').createClient({
-  key: process.env.GOOGLE_API_KEY,
-  Promise: Promise
-})
 
-googleMapsClient.geocode({address: 'denver, co'})
-  .asPromise()
-  .then((response) => {
-    console.log(response.json.results)
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+class GoogleMaps {
+  constructor() {
+    this.googleMapsClient = require('@google/maps').createClient({
+      key: process.env.GOOGLE_API_KEY,
+      Promise: Promise
+    })
+  }
+
+  geocode(location) {
+    this.googleMapsClient.geocode({address: location})
+    .asPromise()
+    .then((response) => {
+      return this._processed(response.json.results)
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+  }
+
+  _processed(response) {
+    let firstResult = response[0]
+    return {
+      lat: firstResult.geometry.location.lat,
+      long: firstResult.geometry.location.lng,
+      formatted_address: firstResult.formatted_address
+    }
+  }
+}
